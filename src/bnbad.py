83f97a088090988d691abcd490f6abf1c02f3adc
Tim Menzies <timm@ieee.org>
2021-01-01 20:27:34 -0500

    saving

--- src/bnbad.py
+++ src/bnbad.py
@@ -2,7 +2,7 @@
 # [home](http://menzies.us/bnbab2)         :: [lib](http://menzies.us/bnbad2/lib.html) ::
 # [cols](http://menzies.us/bnbad2/tab.html) :: [tbl](http://menzies.us/bnbad2/grow.html) 
 # <hr>
-# <a href="http://github.com/timm/bnbad2"><img src="https://github.com/tim/bnbad2/raw/main/etc/img/banner.png" align=left></a>
+# <a href="http://github.com/timm/bnbad2"><img src="https://github.com/timm/bnbad2/raw/main/etc/img/banner.png" align=left></a>
 # [![DOI](https://zenodo.org/badge/318809834.svg)](https://zenodo.org/badge/latestdoi/318809834)  
 # ![](https://img.shields.io/badge/platform-osx%20,%20linux-orange)    
 # ![](https://img.shields.io/badge/language-python3,bash-blue)  
