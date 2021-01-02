6300d78c70be811de53228f9a07cb4f59de4af53
Tim Menzies <timm@ieee.org>
2021-01-01 20:23:35 -0500

    saving

--- src/bnbad.py
+++ src/bnbad.py
@@ -1,17 +1,15 @@
-#  <img width=75 src="https://github.com/timm/keys/raw/main/etc/img/lib.png">     
-#  "Keys = cluster, discretize, elites, contrast"   
-#  [home](http://menzies.us/keys)         :: [lib](http://menzies.us/keys/lib.html) ::
-#  [cols](http://menzies.us/keys/cols.html) :: [tbl](http://menzies.us/keys/tbl.html) ::
-#  [learn](http://menzies.us/keys/learn.html)
-#  <hr>
-#  <a href="http://github.com/timm/keys"><img src="https://github.blog/wp-content/uploads/2008/12/forkme_left_red_aa0000.png?resize=149%2C149" align=left></a>
-#  [![DOI](https://zenodo.org/badge/318809834.svg)](https://zenodo.org/badge/latestdoi/318809834)  
-#  ![](https://img.shields.io/badge/platform-osx%20,%20linux-orange)    
-#  ![](https://img.shields.io/badge/language-lua,bash-blue)  
-#  ![](https://img.shields.io/badge/purpose-ai%20,%20se-blueviolet)  
-#  [![Build Status](https://travis-ci.com/timm/keys.svg?branch=main)](https://travis-ci.com/timm/keys)   
-#  ![](https://img.shields.io/badge/license-mit-lightgrey)
-#--------
+# Find and combine interesting bits. Repeat.
+# [home](http://menzies.us/bnbab2)         :: [lib](http://menzies.us/bnbad2/lib.html) ::
+# [cols](http://menzies.us/bnbad2/tab.html) :: [tbl](http://menzies.us/bnbad2/grow.html) 
+# <hr>
+# <a href="http://github.com/timm/bnbad2"><img src="https://github.com/tim/bnbad2/raw/main/etc/img/banner.png" align=left></a>
+# [![DOI](https://zenodo.org/badge/318809834.svg)](https://zenodo.org/badge/latestdoi/318809834)  
+# ![](https://img.shields.io/badge/platform-osx%20,%20linux-orange)    
+# ![](https://img.shields.io/badge/language-python3,bash-blue)  
+# ![](https://img.shields.io/badge/purpose-ai%20,%20se-blueviolet)  
+#  [![Build Status](https://travis-ci.com/timm/bnbad2.svg?branch=main)](https://travis-ci.com/timm/bnbad2)   
+# ![](https://img.shields.io/badge/license-mit-lightgrey)
+# --------
 
 from lib import o, Thing
 
