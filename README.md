3afbe04343f96fffa3a12e3d00ddacf777db38df
Tim Menzies <timm@ieee.org>
2021-01-03 00:30:29 -0500

    saving

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ Find interesting bits. Combine them. Repeat.<br>
 [cols](http://menzies.us/bnbad2/tab.html) :: [tbl](http://menzies.us/bnbad2/grow.html)<br>
 <hr>
 <a href="http://github.com/timm/bnbad2"><img src="https://github.com/timm/bnbad2/raw/main/etc/img/banner.png" align=left></a>
-[![DOI](https://zenodo.org/badge/326061406.svg)](https://zenodo.org/badge/latestdoi/326061406)<br>
-![](https://img.shields.io/badge/platform-osx%20,%20linux-orange)<br>
-![](https://img.shields.io/badge/language-python3,bash-blue)<br>
-![](https://img.shields.io/badge/purpose-ai%20,%20se-blueviolet)<br>
-[![Build Status](https://travis-ci.com/timm/bnbad2.svg?branch=main)](https://travis-ci.com/timm/bnbad2)<br>
-![](https://img.shields.io/badge/license-mit-lightgrey)
---------
+<a href="https://zenodo.org/badge/latestdoi/326061406"><img src="https://zenodo.org/badge/326061406.svg"></a><br>
+<img src="https://img.shields.io/badge/platform-osx%20,%20linux-orange"><br>
+<img src="https://img.shields.io/badge/language-python3,bash-blue"><br>
+<img src="https://img.shields.io/badge/purpose-ai%20,%20se-blueviolet"><br>
+<a href="https://travis-ci.com/timm/bnbad2"><img src="https://travis-ci.com/timm/bnbad2.svg?branch=main"></a><br>
+<img src="https://img.shields.io/badge/license-mit-lightgrey">
+<hr>
 
 Fun
+
 
--- bnbad2/it.py
+++ bnbad2/it.py
@@ -4,28 +4,28 @@
 # [cols](http://menzies.us/bnbad2/tab.html) :: [tbl](http://menzies.us/bnbad2/grow.html)<br>
 # <hr>
 # <a href="http://github.com/timm/bnbad2"><img src="https://github.com/timm/bnbad2/raw/main/etc/img/banner.png" align=left></a>
-# [![DOI](https://zenodo.org/badge/326061406.svg)](https://zenodo.org/badge/latestdoi/326061406)<br>
-# ![](https://img.shields.io/badge/platform-osx%20,%20linux-orange)<br>
-# ![](https://img.shields.io/badge/language-python3,bash-blue)<br>
-# ![](https://img.shields.io/badge/purpose-ai%20,%20se-blueviolet)<br>
-# [![Build Status](https://travis-ci.com/timm/bnbad2.svg?branch=main)](https://travis-ci.com/timm/bnbad2)<br>
-# ![](https://img.shields.io/badge/license-mit-lightgrey)
-# --------
+# <a href="https://zenodo.org/badge/latestdoi/326061406"><img src="https://zenodo.org/badge/326061406.svg"></a><br>
+# <img src="https://img.shields.io/badge/platform-osx%20,%20linux-orange"><br>
+# <img src="https://img.shields.io/badge/language-python3,bash-blue"><br>
+# <img src="https://img.shields.io/badge/purpose-ai%20,%20se-blueviolet"><br>
+# <a href="https://travis-ci.com/timm/bnbad2"><img src="https://travis-ci.com/timm/bnbad2.svg?branch=main"></a><br>
+# <img src="https://img.shields.io/badge/license-mit-lightgrey">
+# <hr>
 
 from .boot import *
 
-def help(): return [
-    arg("char for less", less="<"),
-    arg("char for more", more=">"),
-    arg("char for skip", skip="?"),
-    arg("char for klass", klass="!"),
-    arg("char for symbols", sym="_"),
-    arg("char for numerics", num=":"),
-    arg("some epsilon", eps=.35),
-    arg("some min", min=.5),
-    arg("some want", want=128),
-    arg("table samples", samples=64),
-    arg("run demos", demos=False)
+def help(): h = arg; return [
+    h("char for less", less="<"),
+    h("char for more", more=">"),
+    h("char for skip", skip="?"),
+    h("char for klass", klass="!"),
+    h("char for symbols", sym="_"),
+    h("char for numerics", num=":"),
+    h("some epsilon", eps=.35),
+    h("some min", min=.5),
+    h("some want", want=128),
+    h("table samples", samples=64),
+    h("run demos", demos=False)
 ]
 
 it = o(**{k: d for k, d, _ in help()})
