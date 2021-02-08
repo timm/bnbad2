443d2f8b4bb3f58d39e53ac52be48c7464e5e8d1
Tim Menzies <timm@ieee.org>
2021-02-08 00:04:02 -0500

    saving

--- meta.py
+++ meta.py
@@ -1,29 +1,26 @@
-import types
-class Box:
+class o:
   "For classes that are mostly data stores, with few (or no) methods."
+  def __init__(i, **d): i.__dict__.update(d)
   def __repr__(i): return i.__class__.__name__ + str(
       {k: v for k, v in sorted(i.__dict__.items()) if k[0] != "_"})
 
-def _so(**d):
-  def funp(x): return isinstance(x, types.FunctionType)
-  name, *keys = list(d.keys())
-  data = {k: d[k] for k in keys if not funp(d[k])}
-  klass = globals()[name] = type(name, tuple([d[name]]), {})
-  setattr(klass, "__init__",
-          lambda i, **kw: i.__dict__.update({**data, **kw}))
-  for k in d:
-    if funp(d[k]):
-      setattr(klass, k, d[k])
+def _so(name, d, f):
+  d, f = d.__dict__, f.__dict__
+  k = globals()[name] = type(name, tuple([o]), {})
+  setattr(k, "__init__",
+          lambda i, ** kw: i.__dict__.update({**d, **kw}))
+  [setattr(k, x, f[x]) for x in f]
 
 def _do(c): return lambda f: setattr(c, f.__name__, lambda i,
                                      *l, **d: f(i, *l, **d))
 
 
 if __name__ == "__main__":
-  _so(Robot=Box, model=1970, n=2, hi=100,
-      __lt__=lambda i, j: i.model < j.model)
+  print(o(a=1, b=2))
+  _so("Robot", o(model=1970, n=2, hi=100),
+      o(__lt__=lambda i, j: i.model < j.model))
   # @_do(Robot)
-  #def __lt__(i, j): return i.model < j.model
+  # def __lt__(i, j): return i.model < j.model
   assert str(Robot()) == "Robot{'hi': 100, 'model': 1970, 'n': 2}"
   r = Robot(model=34) # can override defaults
   assert r.n == 2     # will build with local defaults
