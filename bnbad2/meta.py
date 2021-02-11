6f25ed20f9562090712420d7ae3c105119bb8334
Tim Menzies <timm@ieee.org>
2021-02-11 09:32:24 -0500

    saving

--- bnbad2/meta.py
+++ bnbad2/meta.py
@@ -1,24 +1,26 @@
-import types
-import copy
-
-def fun(x): return isinstance(x, types.FunctionType)
+from copy import deepcopy as cp
 
 class o:
   def __init__(i, **d): i.__dict__.update(**d)
   def __repr__(i): return str(
-      {k: (v.__name__ + "()" if fun(v) else v)
+      {k: (v.__name__ + "()" if callable(v) else v)
        for k, v in sorted(i.__dict__.items()) if k[0] != "_"})
 
-def so(**d):
-  i = o()
-  i.__dict__ = {k: ((lambda *l, **kw: v(i, *l, **kw)) if fun(v) else v)
-                for k, v in d.items()}
+def of(i, **methods):
+  def method(f): return lambda *l, **d: f(i, *l, **d)
+  for k, f in methods.items():
+    i.__dict__[k] = method(f)
   return i
 
 def Fred(a=2):
-  def p(i, n=100): print(str(i.a + i.c + n))
-  return so(a=a, b=2, c=3, say=p)
+  def p(i): return i.a + i.c + 20
+  def lt(i, j): return i.a < j.a
+  return of(o(a=a, b=2, c=3), say=p, lt=lt)
 
 
 f = Fred(a=-10000000000000000)
-f.say(100000000)
+print(f.say())
+print(f.lt(Fred()))
+# f.say(100000000)
+# g = Fred(10)
+# print(g < f
